8e33233f5252b94418c6f70f14883f49d6dae06b
Tzelos <george.sot@windowslive.com>
2024-02-19 22:04:02 +0200

    add live gps position tracking

--- app/src/main/java/com/example/treasuregame/GameActivity.java
+++ app/src/main/java/com/example/treasuregame/GameActivity.java
@@ -1,18 +1,32 @@
 package com.example.treasuregame;
 
 import Models.Game;
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.*;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.Locale;
+
+public class GameActivity extends AppCompatActivity implements LocationListener {
+
     private TextView textViewGame;
 
     ObjectMapper objectMapper = new ObjectMapper();
     Game game;
+
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,7 @@ public class GameActivity extends AppCompatActivity {
         Game game = (Game) getIntent().getSerializableExtra("Game");
         textViewGame = findViewById(R.id.textViewGame);
         textViewGame.setText(game.toString());
+        getLocation();
 //        try {
 //            game = objectMapper.readValue(gamestring, Game.class);
 //            textView.findViewById(R.id.textView);
@@ -29,9 +44,79 @@ public class GameActivity extends AppCompatActivity {
 //        } catch (JsonProcessingException e) {
 //            throw new RuntimeException(e);
 //        }
+        if (ContextCompat.checkSelfPermission(GameActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(GameActivity.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },100);
+        }
+
+
+
     }
+
+
+
 
     public void TerminateGame(View view) {
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, GameActivity.this);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        try {
+            getLocation();
+//            Geocoder geocoder = new Geocoder(GameActivity.this, Locale.getDefault());
+//            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+//            String address = addresses.get(0).getAddressLine(0);
+
+//            textViewGame.setText(location.getLatitude()+" + "+location.getLongitude());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
